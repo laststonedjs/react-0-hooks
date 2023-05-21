3d8c1f21bd0b066358854d1a108ab9c0503a7530
Filip <filipsarovic428@gmail.com>
2023-05-21 22:18:26 +0200

    Added the functionality for adding a new User

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react'
 // components
 import UserTable from './tables/UserTable'
+import AddUserForm from './forms/AddUserForm';
 // data
 import { usersData } from './data/dummy';
 
 const App = () => {
   const [users, setUsers] = useState(usersData);
+
+  // this function take a 'user' object as a parameter and add them to the users array of objects
+  const addUser = (user) => {
+    user.id = users.length + 1;
+    setUsers([...users, user]);
+  }
 
   return (
     <div className="container">
@@ -13,6 +20,7 @@ const App = () => {
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add user</h2>
+          <AddUserForm addUser={addUser} />
         </div>
         <div className="flex-large">
           <h2>View users</h2>
